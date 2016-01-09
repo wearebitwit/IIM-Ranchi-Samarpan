ff30a1b5d3c49b2b189d166081ce9ed61af9814f
Nikhil Srivastava <niksrc@gmail.com>
2016-01-10 05:28:39 +0530

    Adds updated views

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Samarpan | IIM Ranchi</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/animate.css" rel="stylesheet">	
+	<link href="css/responsive.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+	    <script src="js/html5shiv.js"></script>
+	    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+	<header id="header" role="banner">		
+		<div class="main-nav">
+			<div class="container">
+				<div class="header-top">
+					<div class="pull-right social-icons">
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-google-plus"></i></a>
+						<a href="#"><i class="fa fa-youtube"></i></a>
+					</div>
+				</div>     
+		        <div class="row">	        		
+		            <div class="navbar-header">
+		                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		                    <span class="sr-only">Toggle navigation</span>
+		                    <span class="icon-bar"></span>
+		                    <span class="icon-bar"></span>
+		                    <span class="icon-bar"></span>
+		                </button>
+		                <a class="navbar-brand" href="index.html">
+		                	<img class="img-responsive" src="images/logo.png" alt="logo">
+		                </a>                    
+		            </div>
+		            <div class="collapse navbar-collapse">
+		                <ul class="nav navbar-nav navbar-right">                 
+		                    <li class="scroll active"><a href="#home">Home</a></li>
+		                    <li class="scroll"><a href="#explore">Explore</a></li>                         
+		                    <li class="scroll"><a href="#event">Event</a></li>
+		                    <li class="scroll"><a href="#about">About</a></li>                     
+		                    <li class="scroll"><a href="#media">Media</a></li>
+		                    <li><a class="scroll" href="#initiatives" >Initiatives so far</a></li>
+		                    <li class="scroll"><a href="#contact">Contact</a></li>       
+		                </ul>
+		            </div>
+		        </div>
+	        </div>
+        </div>                    
+    </header>
+    <!--/#header--> 
+
+    <section id="home">	
+		<div id="main-slider" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#main-slider" data-slide-to="0" class="active"></li>
+				<li data-target="#main-slider" data-slide-to="1"></li>
+				<li data-target="#main-slider" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+
+                <div class="item  active">
+                    <img class="img-responsive" src="images/slider/back3.jpg" alt="slider">	
+                    <div class="carousel-caption">
+                        <h2>Nirvaadh</h2>
+                        <h4>Radix | Fun-a-thon &amp walk-a-thon | Excelsior</h4>
+                        <a href="#event" class="scroll">register here <i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div>	
+				<div class="item">
+					<img class="img-responsive" src="images/slider/back1.jpg" alt="slider">						
+					<div class="carousel-caption">
+						<h2>An evening with YUWA </h2>
+                        <h4>Yuwa uses girls’ team sports as a<br> platform for social development in rural India.</h4>
+<!--						<a href="#contact"><i class="fa fa-angle-right"></i></a>-->
+					</div>
+				</div>
+				<div class="item">
+					<img class="img-responsive" src="images/slider/back2.jpg" alt="slider">	
+					<div class="carousel-caption">
+						<h2>Sahayak - Wish Fulfilment </h2>
+						<h4>An initiative by samarpan</h4>
+<!--						<a href="#contact">GRAB YOUR TICKETS <i class="fa fa-angle-right"></i></a>-->
+					</div>
+				</div>			
+			</div>
+			
+            <!-- Controls -->
+            <a class="left carousel-control" href="#main-slider" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#main-slider" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+			
+		</div>    	
+    </section>
+	<!--/#home-->
+
+	<section id="explore">
+		<div class="container">
+			<div class="row">
+				<div class="watch">
+					<img class="img-responsive" src="images/watch.png" alt="">
+				</div>				
+				<div class="col-md-4 col-md-offset-2 col-sm-5">
+					<h2>NIRVAADH starts in..</h2>
+				</div>				
+				<div class="col-sm-7 col-md-6">					
+					<ul id="countdown">
+						<li>					
+							<span class="days time-font">00</span>
+							<p>days </p>
+						</li>
+						<li>
+							<span class="hours time-font">00</span>
+							<p class="">hours </p>
+						</li>
+						<li>
+							<span class="minutes time-font">00</span>
+							<p class="">minutes</p>
+						</li>
+						<li>
+							<span class="seconds time-font">00</span>
+							<p class="">seconds</p>
+						</li>				
+					</ul>
+				</div>
+			</div>
+			<div class="cart">
+				<a href="/events"><i class="fa fa-shopping-cart"></i> <span>Register here</span></a>
+			</div>
+		</div>
+	</section><!--/#explore-->
+
+	<section id="event">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-9">
+					<div id="event-carousel" class="carousel slide" data-interval="false">
+                        <h2 class="heading">Events (6-7th February 2016)</h2>
+						<div class="carousel-inner">
+							<div class="item active">
+								<div class="row event-row">							
+									<div class="col-sm-4">
+									<a href="/events">
+										<div class="single-event">
+											<img class="img-responsive" src="images/event/event1.jpg" alt="event-image">
+                                            <h4>Radix</h4>
+                                            <h5>‘Ideas to Implementation’<br>“Sustainable Business Strategies”
+                                            </h5>
+										</div>
+                                        </a>
+									</div>
+									<div class="col-sm-4">
+                                        <a href="/events">
+										<div class="single-event">
+											<img class="img-responsive" src="images/event/event2.jpg" alt="event-image">
+                                            <h4>Funathon and Walkathon – 2.0
+                                            </h4>
+                                            <h5>Raise funds for Yuwa for construction of its new campus
+                                            </h5>
+										</div>
+                                        </a>
+									</div>
+									<div class="col-sm-4">
+                                        <a href="/events">
+										<div class="single-event">
+											<img class="img-responsive" src="images/event/event3.jpg" alt="event-image">
+                                            <h4>Excelsior</h4>
+                                            <h5>Football Matches | Musical Band performance | Musical-Skit performance </h5>
+										</div>
+                                        </a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="guitar">
+					<img class="img-responsive" src="images/podium.png" alt="guitar">
+				</div>
+			</div>			
+		</div>
+	</section><!--/#event-->
+
+	<section id="about">
+		<div class="guitar2">				
+			<img class="img-responsive" src="images/about-bg.png" alt="guitar">
+		</div>
+		<div class="about-content">					
+					<h2>About Samarpan</h2>
+            <p>Samarpan is the Corporate Social Responsibility club of IIM Ranchi. “Samarpan” is homage to all those who have demonstrated commendable spirit and courage to take the cause of mankind forward and a show of solidarity to those who have been victimized or have suffered due to the sharp divide that exists in our society.<br> 
+
+                Since its inception, the club has taken several initiatives like Samvid (cleanliness awareness to motivate the rural people to keep the environment clean), Volunteer to Teach, National Conference on Inclusive and Impactful CSR, Blood donation camp (organized on Independence day and Republic day), Joy of Giving (fulfilling wishes of underprivileged section of society), Funathon and Walkathon (Run for a cause) etc.<br>
+
+                We strongly believe in inclusive growth and developing sustainable solutions for the betterment of society. One small pebble can create ripples in an entire river and we are trying our best to generate those ripples.
+               <br> <strong> Our Vision</strong><br>
+
+                ” One day all corporate houses will work not just for their personal growth, but also for an inclusive and sustainable growth and make this world a better place to live in.”
+<br>
+                <strong>Our Mission</strong><br>
+
+                ” Imbibing, in the managers of tomorrow, a sense of responsibility and sensitivity towards the society in which their businesses will operate”</p>
+<!--					<a href="#" class="btn btn-primary">View Date & Place <i class="fa fa-angle-right"></i></a>-->
+				
+		</div>
+	</section><!--/#about-->
+	
+	<section id="media">
+	    <h2>Media</h2>
+	    <div class="container">
+	        <div class="row">
+	            <div class="col-md-4">
+	                <img class="img-responsive" src="images/media/1.jpg">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et laoreet nisi. Nam nibh purus, feugiat a pellentesque maximus, maximus sed quam. Etiam congue suscipit efficitur. Mauris sed arcu ac quam efficitur hendrerit. Ut vulputate tincidunt erat, ut hendrerit ligula maximus molestie. Nulla sed nisl eu tellus pretium fringilla aliquam sit amet ipsum. Etiam id mattis sapien.</p>
+	            </div>
+	            <div class="col-md-4">
+	                <img class="img-responsive" src="images/media/2.jpg">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et laoreet nisi. Nam nibh purus, feugiat a pellentesque maximus, maximus sed quam. Etiam congue suscipit efficitur. Mauris sed arcu ac quam efficitur hendrerit. Ut vulputate tincidunt erat, ut hendrerit ligula maximus molestie. Nulla sed nisl eu tellus pretium fringilla aliquam sit amet ipsum. Etiam id mattis sapien.</p>
+	            </div>
+	            <div class="col-md-4">
+	                <img class="img-responsive" src="images/media/1.jpg">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et laoreet nisi. Nam nibh purus, feugiat a pellentesque maximus, maximus sed quam. Etiam congue suscipit efficitur. Mauris sed arcu ac quam efficitur hendrerit. Ut vulputate tincidunt erat, ut hendrerit ligula maximus molestie. Nulla sed nisl eu tellus pretium fringilla aliquam sit amet ipsum. Etiam id mattis sapien.</p>
+	            </div>
+	        </div>
+	    </div>
+	</section><!--/#media-feed-->
+	
+	<section id="initiatives">
+		<div id="initiatives-carousel" class="carousel slide" data-interval="false">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-10">
+						<h2>initiatives taken so far</h2>			
+						<a class="initiatives-control-left" href="#initiatives-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+						<a class="initiatives-control-right" href="#initiatives-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a>
+						<div class="carousel-inner">
+							<div class="item active">
+								<ul>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor1.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor2.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor3.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor4.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor5.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor6.png" alt=""></a></li>
+								</ul>
+							</div>
+							<div class="item">
+								<ul>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor6.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor5.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor4.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor3.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor2.png" alt=""></a></li>
+									<li><a href="#"><img class="img-responsive" src="images/sponsor/sponsor1.png" alt=""></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>				
+			</div>
+			<div class="light">
+				<img class="img-responsive" src="images/light.png" alt="">
+			</div>
+		</div>
+	</section><!--/#initiatives-->
+
+	<section id="contact">
+		<div id="map">
+			<div id="gmap-wrap">
+	 			<div id="gmap"> 				
+	 			</div>	 			
+	    	</div>
+		</div><!--/#map-->
+		<div class="contact-section">
+			<div class="ear-piece">
+				<img class="img-responsive" src="images/ear-piece.png" alt="">
+			</div>
+			<div class="container">
+                <div class="row">
+                    <div class="contact-text col-sm-5 col-sm-offset-3">
+                        <h3>Contact</h3>
+                    </div>
+					<div class="col-sm-3 col-sm-offset-3">
+						
+						<div class="contact-address">
+							<address>
+                                Pratishruti Jain<br>
+
+                                Junior Coordinator – Samarpan club<br>
+
+                                Ph: +91- 9632597603<br>
+
+                                Email: pratishruti.j15@iimranchi.ac.in
+							</address>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						
+						<div class="contact-address">
+							<address>
+                                Revati Gaurav<br>
+
+                                Junior Coordinator – Samarpan club<br>
+
+                                Ph: +91- 9022446595<br>
+
+                                Email: revati.gurav15@iimranchi.ac.in
+							</address>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						
+						<div class="contact-address">
+							<address>
+                                Athina<br>
+
+                                Junior Coordinator – Samarpan club<br>
+
+                                Ph: +91- 7091098550<br>
+
+                                Email: chau.chauhai15@iimranchi.ac.in
+							</address>
+						</div>
+					</div>
+					<div class="col-sm-2">
+					</div>
+				</div>
+			</div>
+		</div>		
+	</section>
+    <!--/#contact-->
+
+    <footer id="footer">
+        <div class="container">
+            <div class="text-center">
+                <p> Copyright  &copy; 2016 <a target="_blank" href="#">Samarpan IIM Ranchi</a>  <br>Revamped by <a href="http://www.wearebitwit.com">Team BitWit</a></p>                
+            </div>
+        </div>
+    </footer>
+    <!--/#footer-->
+  
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+  	<script type="text/javascript" src="js/gmaps.js"></script>
+	<script type="text/javascript" src="js/smoothscroll.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax.js"></script>
+    <script type="text/javascript" src="js/coundown-timer.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo.js"></script>
+    <script type="text/javascript" src="js/jquery.nav.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>  
+</body>
+</html>
